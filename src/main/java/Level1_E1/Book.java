c4efcdc1c6100c1cb788655bd5427a27c68edf99
crist <cristhianchulca49@gmail.com>
2025-11-20 17:23:27 +0100

    feat: add compareTo override to sort books alphabetically by title

--- src/main/java/Level1_E1/Book.java
+++ src/main/java/Level1_E1/Book.java
@@ -1,10 +1,10 @@
 package Level1_E1;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
     private String author;
 
-    public  Book(String title, String author) {
+    public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
@@ -14,11 +14,16 @@ public class Book {
     }
 
     @Override
-    public boolean equals(Object object){
-        if(this == object){
+    public int compareTo(Book other) {
+        return this.title.compareTo(other.title);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
             return true;
         }
-        if(object == null || getClass() != object.getClass()){
+        if (object == null || getClass() != object.getClass()) {
             return false;
         }
         Book book = (Book) object;
@@ -26,7 +31,7 @@ public class Book {
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return this.title.hashCode();
     }
 }
