1f6eea98a3d1342366e19454071e1bc73fdcc9a2
crist <cristhianchulca49@gmail.com>
2025-11-24 22:41:59 +0100

    test: add testNullBookshop() to verify Bookshop is not null and not empty on initialization

--- src/test/java/Level1_E1/TestBookshop.java
+++ src/test/java/Level1_E1/TestBookshop.java
@@ -1,6 +1,8 @@
 package Level1_E1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBookshop {
     Book book;
@@ -10,5 +12,13 @@ public class TestBookshop {
     public void setUp() {
         book = new Book("Book 1", "Author 1");
         bookshop = new Bookshop(book);
+    }
+
+    @Test
+    void testNullBookshop() {
+        assertAll(
+                () -> assertFalse(bookshop.getBooks().isEmpty()),
+                () -> assertNotNull(bookshop.getBooks())
+        );
     }
 }
