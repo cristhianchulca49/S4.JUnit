66fa8c908629dfa75f26f775fc6cf84be47cf64c
crist <cristhianchulca49@gmail.com>
2025-11-24 23:49:58 +0100

    test: add TestCalculationDNI to validate that returns the correct letter

--- src/test/java/Level1_E2/TestCalculationDNI.java
+++ src/test/java/Level1_E2/TestCalculationDNI.java
@@ -1,4 +1,13 @@
 package Level1_E2;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCalculationDNI {
+
+    @Test
+    public void testCalculationDNI() {
+        CalculationDNI calculationDNI = new CalculationDNI();
+        assertEquals("Z", calculationDNI.calculateLetter(12345678));
+    }
 }
