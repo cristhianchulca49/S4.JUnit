be2e87b8eb0c24777bce8134b6731ddcdbf5a594
crist <cristhianchulca49@gmail.com>
2025-11-24 15:33:44 +0100

    feat: add getBookTitleByIndex() method with validation and user-to-internal index conversion

--- src/main/java/Level1_E1/Bookshop.java
+++ src/main/java/Level1_E1/Bookshop.java
@@ -1,8 +1,6 @@
 package Level1_E1;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Bookshop {
     private Set<Book> books = new TreeSet<>();
@@ -13,5 +11,13 @@ public class Bookshop {
 
     public Set<Book> getBooks() {
         return Collections.unmodifiableSet(books);
+    }
+
+    public String getBookTitleByIndex(int index) {
+        if (index <= 0 || index > books.size()) {
+            return null;
+        }
+        List<Book> bookList = new ArrayList<>(books);
+        return bookList.get(index-1).getTitle();
     }
 }
