3d53173f231adb79e4e1e8a40937489402c0f9f0
crist <cristhianchulca49@gmail.com>
2025-11-24 22:08:28 +0100

    test: add TestBookshop with book and bookshop fields plus setUp() initialization

---
+++ src/test/java/Level1_E1/TestBookshop.java
@@ -0,0 +1,14 @@
+package Level1_E1;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBookshop {
+    Book book;
+    Bookshop bookshop;
+
+    @BeforeEach
+    public void setUp() {
+        book = new Book("Book 1", "Author 1");
+        bookshop = new Bookshop(book);
+    }
+}
