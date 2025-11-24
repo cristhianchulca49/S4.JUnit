d7f9fa86c7e943ea72fc2bca44c57de730b73d89
crist <cristhianchulca49@gmail.com>
2025-11-24 23:25:14 +0100

    feat: add CalculationDNI constructor with whole DNI letters

--- src/main/java/Level1_E2/CalculationDNI.java
+++ src/main/java/Level1_E2/CalculationDNI.java
@@ -1,8 +1,36 @@
 package Level1_E2;
 
-public class CalculationDNI {
-    static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CalculationDNI {
+
+    private List<String> letter = new ArrayList<>();
+
+    public CalculationDNI() {
+        letter.add("T");
+        letter.add("R");
+        letter.add("W");
+        letter.add("A");
+        letter.add("G");
+        letter.add("M");
+        letter.add("Y");
+        letter.add("F");
+        letter.add("P");
+        letter.add("D");
+        letter.add("X");
+        letter.add("B");
+        letter.add("N");
+        letter.add("J");
+        letter.add("Z");
+        letter.add("S");
+        letter.add("Q");
+        letter.add("V");
+        letter.add("H");
+        letter.add("L");
+        letter.add("C");
+        letter.add("K");
+        letter.add("E");
     }
 }
 
