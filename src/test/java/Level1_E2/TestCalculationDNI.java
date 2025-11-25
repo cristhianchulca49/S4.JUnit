cdd04fabd299537e0520713fb64b72042c400cae
crist <cristhianchulca49@gmail.com>
2025-11-25 12:24:03 +0100

    test: refactor to use @ParameterizedTest for calculating 10 DNI numbers

--- src/test/java/Level1_E2/TestCalculationDNI.java
+++ src/test/java/Level1_E2/TestCalculationDNI.java
@@ -1,13 +1,25 @@
 package Level1_E2;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCalculationDNI {
 
-    @Test
-    public void testCalculationDNI() {
-        CalculationDNI calculationDNI = new CalculationDNI();
-        assertEquals("Z", calculationDNI.calculateLetter(12345678));
+    @ParameterizedTest
+    @CsvSource({
+            "57483920,C",
+            "18492755,L",
+            "90321487,G",
+            "45219833,Q",
+            "76152409,F",
+            "62841755,M",
+            "39752184,L",
+            "84512977,Y",
+            "21645739,W",
+            "68974512,L"
+    })
+    public void testCalculationDNI(int number, String expectedLetter) {
+        assertEquals(expectedLetter, CalculationDNI.calculateLetter(number));
     }
 }
