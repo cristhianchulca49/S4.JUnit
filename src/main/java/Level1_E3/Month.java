d50e9fd4dda6f058fdec369f88c2a97c22f6aec5
crist <cristhianchulca49@gmail.com>
2025-11-25 11:05:27 +0100

    feat: add constructor with months and getMonth() method handling IndexOutOfBoundsException

--- src/main/java/Level1_E3/Month.java
+++ src/main/java/Level1_E3/Month.java
@@ -1,5 +1,30 @@
 package Level1_E3;
 
-public class Month {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Month {
+    private List<String> months = new ArrayList();
+
+    public Month() {
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+    }
+
+    public String getMonth(int index) {
+        if(index < 0 || index >= months.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + months.size());
+        }
+        return months.get(index-1);
+    }
 }
