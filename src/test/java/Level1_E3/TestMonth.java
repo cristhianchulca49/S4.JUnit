4ec211e37fef43abba637c4746bea9c359379e9b
crist <cristhianchulca49@gmail.com>
2025-11-25 11:13:41 +0100

    test: add testMonthThrowsException() for ArrayIndexOutOfBoundsException

--- src/test/java/Level1_E3/TestMonth.java
+++ src/test/java/Level1_E3/TestMonth.java
@@ -1,4 +1,15 @@
 package Level1_E3;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestMonth {
+    @Test
+    public void testMonthThrowsException() {
+        Month month = new Month();
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            month.getMonth(99);
+        });
+    }
 }
