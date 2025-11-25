6620cad4b6e76d77f6157cbdc771a37858b225af
crist <cristhianchulca49@gmail.com>
2025-11-25 11:17:13 +0100

    refactor: remove unused exception and delete Main class

--- src/main/java/Level1_E1/Exceptions/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package Level1_E1.Exceptions;
-
-public class DuplicateException extends Exception {
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
--- src/main/java/Level1_E1/Main.java
+++
@@ -1,7 +0,0 @@
-package Level1_E1;
-
-public class Main {
-    static void main(String[] args) {
-
-    }
-}
